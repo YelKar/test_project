cf7a3f709c93147b7a3341dce02df890add6f6d0
yelisey <karamyshev_e@1060.ru>
2023-02-07 15:35:24 +0300

    01.02

--- exam/tasks/task23.py
+++ exam/tasks/task23.py
@@ -8,4 +8,26 @@ def task3607(n: int):
     return 0
 
 
-print(task3607(50))
+def task13471(n: int):
+    if n == 24:
+        return 0
+    if n == 1:
+        return 1
+    elif (n - 1) % 2 == 0:
+        return task13471(n - 1) + task13471((n - 1) // 2)
+    elif n:
+        return task13471(n - 1)
+    return 0
+
+
+def task5977(n: int):
+    if n == 10:
+        return 1
+    elif n < 10:
+        return 0
+    elif n > 10:
+        return task5977(n - 10) + task5977(n - 1)
+    return task5977(n - 1)
+
+
+print(task5977(33))
