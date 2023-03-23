08c7f922b2c121d5cef328333f0a529faad5d035
Yel_manjaro_vm <karamyshev_e@1060.ru>
2023-03-23 22:03:34 +0300

    dynamicLib fibonacci

--- dynamicLibs/module.c
+++ dynamicLibs/module.c
@@ -1,13 +1,30 @@
 #include <Python.h>
 #include "stdio.h"
 
+// fib(max_num) more than max unsigned long long number
+#define max_num 94
+unsigned long long mem[max_num];
 
-PyObject* func() {
-    return PyLong_FromLong(0);
+
+unsigned long long _fib(unsigned long long n) {
+    unsigned long long m = mem[n];
+    if (m) {
+        return m;
+    }
+    if (n <= 2) {
+        return 1;
+    }
+    return mem[n] = _fib(n - 1) + _fib(n - 2);
+}
+
+
+PyObject* fib(PyObject*, PyObject * n) {
+    unsigned long long num = PyLong_AsUnsignedLongLong(n);
+    return PyLong_FromUnsignedLongLong(_fib(num));
 }
 
 static PyMethodDef module_methods[] = {
-    { "foo", (PyCFunction)func, METH_VARARGS, NULL },
+    { "fibonacci", (PyCFunction)fib, METH_O, NULL },
     { NULL, NULL, 0, NULL }
 };
 
@@ -20,5 +37,6 @@ static PyModuleDef module = {
 };
 
 PyMODINIT_FUNC PyInit_module() {
+    memset(mem, 0, max_num*sizeof(unsigned long long));
     return PyModule_Create(&module);
 }
--- dynamicLibs/module.pyi
+++ dynamicLibs/module.pyi
@@ -1,2 +1,14 @@
-def foo():
+def fibonacci(n: int) -> int:
+    """
+    return fibonacci number
+    fibonacci -> 1 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 ...
+
+    >>> fibonacci(2)
+    1
+    >>> fibonacci(4) == fibonacci(2) + fibonacci(3)
+    True
+
+    :param n:
+    :return:
+    """
     pass
